dfa1a55b068bccf87b7dda7273009715f881fa51
abdo-giprel <abdo.gebril2000@gmail.com>
2021-05-27 17:36:24 +0200

    test2 branches

--- layout/home/html/home.php
+++ layout/home/html/home.php
@@ -21,7 +21,7 @@
                     <li><a href="\ChatApp">ChatApp</a></li>
                     <li> <a href="#about">About us</a></li>
                     <li><a href="#contact">Contact us </a></li>
-                    <li><a href="#">Exams </a></li>
+                    <li><a href="#">Exams</a></li>
                     
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
